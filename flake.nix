d82190f193e1714a03fdfa1dfb5dd8509c4e49c2
mimvoid <mimvoid@proton.me>
2025-11-05 19:24:45 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Flake for luals-gir";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      allSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
+      toSystems = passPkgs: allSystems (system: passPkgs (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = toSystems (pkgs: {
+        default =
+          let
+            luaEnv = pkgs.luajit.withPackages (
+              ps: with ps; [
+                xml2lua
+                inspect # for debugging
+              ]
+            );
+          in
+          pkgs.mkShell {
+            name = "luals-gir";
+
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            packages = builtins.attrValues {
+              inherit (pkgs) glib gobject-introspection lua-language-server;
+              inherit luaEnv;
+            };
+
+            shellHook = ''
+              export LUA_LS_PATH='${luaEnv}/share/lua/5.1'
+            '';
+          };
+      });
+    };
+}
